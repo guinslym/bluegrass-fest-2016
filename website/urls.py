b950901c178673f85f8e40f24a5009b27fef4b42
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-07-09 01:19:43 -0400

    milestone-- base.urls.py works

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$',
-        TemplateView.as_view(template_name='bluegrassfest/base.html'),
+        TemplateView.as_view(template_name='base.html'),
         name="add_invitation"
         ),
 ]
