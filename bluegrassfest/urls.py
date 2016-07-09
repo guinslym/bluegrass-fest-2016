23466329a88a18e9b3359b499e48a179706718ef
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-07-09 01:22:50 -0400

    adding urls

--- bluegrassfest/urls.py
+++ bluegrassfest/urls.py
@@ -5,4 +5,10 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('website.urls', namespace='website')),
+    """
+    url(r'^$',
+        TemplateView.as_view(template_name='base.html'),
+        name="add_invitation"
+        ),
+    """
 ]
